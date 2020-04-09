78def4c7b3002b9eaa0b9524a0fb11be83dc0afb
Cherkesky <guy@cherkesky.com>
2020-04-09 15:36:46 -0500

    initial code from exercise

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace random
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Random random = new Random();
+      List<int> numbers = new List<int> {
+        random.Next(6),
+        random.Next(6),
+        random.Next(6),
+        random.Next(6),
+        random.Next(6),
+        random.Next(6),
+};
+
+for (int i=0; i<numbers.Count; i++) {
+   // Determine if the current loop index is contained inside of the `numbers` list. Print a message to the console indicating whether the index is in the list.
+   
+}
+    }
+  }
+}
